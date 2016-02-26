58b8453b32603e4c753d3aaa11785c20bf400f09
priyasindkar <sindkar.priya@gmail.com>
2016-02-26 12:03:20 +0530

    gbd

--- app/src/main/java/com/webmyne/connect/leads/PostLeadActivity.java
+++ app/src/main/java/com/webmyne/connect/leads/PostLeadActivity.java
@@ -4,16 +4,22 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.andexert.library.RippleView;
+import com.rengwuxian.materialedittext.MaterialEditText;
 import com.webmyne.connect.R;
 import com.webmyne.connect.Utils.Constants;
 import com.webmyne.connect.base.DrawerActivity;
@@ -32,13 +38,20 @@ public class PostLeadActivity extends AppCompatActivity implements View.OnClickL
     private ImageView imgVertical;
     private FloatingActionButton fab;
     private RippleView txtPostLead;
+    private MaterialEditText editName, editRefNo;
     private String verticalName="", verticalShortName="";
+
+    private ImageView imgSearchContacts;
+    private static final int RESULT_PICK_CONTACT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_lead);
+        init();
+    }
 
+    private void init() {
         if(getIntent()!=null) {
             verticalColorIndex = getIntent().getIntExtra("vertical_color_index", 0);
             selectedVertical = getIntent().getStringExtra("selected_vertical");
@@ -65,13 +78,16 @@ public class PostLeadActivity extends AppCompatActivity implements View.OnClickL
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(this);
         imgVertical = (ImageView) findViewById(R.id.imgVertical);
-       // fab.setBackgroundTintList(ColorStateList.valueOf(ColorGenerator.MATERIAL.getColorAtIndex(verticalColorIndex)));
         TextDrawable drawable2 = TextDrawable.builder().buildRound(verticalShortName, ColorGenerator.MATERIAL.getColorAtIndex(verticalColorIndex));
         imgVertical.setImageDrawable(drawable2);
 
         txtPostLead = (RippleView) findViewById(R.id.txtPostLead);
         txtPostLead.setOnRippleCompleteListener(this);
+        editName = (MaterialEditText) findViewById(R.id.editName);
+        editRefNo = (MaterialEditText) findViewById(R.id.editRefNo);
 
+        imgSearchContacts = (ImageView) findViewById(R.id.imgSearchContacts);
+        imgSearchContacts.setOnClickListener(this);
     }
 
     @Override
@@ -100,6 +116,44 @@ public class PostLeadActivity extends AppCompatActivity implements View.OnClickL
                     }
                 });*/
                 break;
+            case R.id.imgSearchContacts:
+                pickContact(v);
+            break;
+        }
+    }
+
+    public void pickContact(View v)
+    {
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        startActivityForResult(contactPickerIntent, RESULT_PICK_CONTACT);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked(data);
+                    break;
+            }
+        }
+    }
+
+    private void contactPicked(Intent data) {
+        Cursor cursor = null;
+        try {
+            String phoneNo = null ;
+            String name = null;
+            Uri uri = data.getData();
+            cursor = getContentResolver().query(uri, null, null, null, null);
+            cursor.moveToFirst();
+            int  phoneIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            int  nameIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+            phoneNo = cursor.getString(phoneIndex);
+            name = cursor.getString(nameIndex);
+            editRefNo.setText(phoneNo);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -109,7 +163,7 @@ public class PostLeadActivity extends AppCompatActivity implements View.OnClickL
             case R.id.txtPostLead:
                 AlertDialog.Builder builder = new AlertDialog.Builder(PostLeadActivity.this, R.style.MaterialBaseTheme_Light_AlertDialog);
                 builder.setTitle("Lead Successfully Uploaded!");
-                builder.setMessage("An SMS is sent to your contact with further details");
+                builder.setMessage("SMS & Email is sent to your contact with further details");
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
