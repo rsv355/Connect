71f71ea178ef4bc707c31f25f295c714690d827b
krishnakumar <softeng.krishna@gmail.com>
2016-02-29 19:07:38 +0530

    sfgdfg

--- app/src/main/java/com/webmyne/connect/base/MainActivity.java
+++ app/src/main/java/com/webmyne/connect/base/MainActivity.java
@@ -20,9 +20,20 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.webmyne.connect.R;
 import com.webmyne.connect.Utils.Functions;
 import com.webmyne.connect.customUI.viewPager.DotsView;
@@ -31,6 +42,8 @@ import com.webmyne.connect.customUI.viewPager.SCViewAnimation;
 import com.webmyne.connect.customUI.viewPager.SCViewAnimationUtil;
 import com.webmyne.connect.customUI.viewPager.SCViewPager;
 import com.webmyne.connect.customUI.viewPager.SCViewPagerAdapter;
+
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -42,6 +55,36 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     private SCViewPagerAdapter mPageAdapter;
     private DotsView mDotsView;
     CallbackManager callbackManager;
+    ProfileTracker profileTracker;
+    GoogleApiClient mGoogleApiClient;
+    private int RC_SIGN_IN = 101;
+
+    private void initScoialLogins(){
+        // Intialize Facebook
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult connectionResult) {
+                        Toast.makeText(MainActivity.this, "Google+ Connection Failed.", Toast.LENGTH_SHORT).show();
+                    }
+                } /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                Log.e("currentProfile",""+currentProfile.getId());
+                Log.e("currentProfile",""+currentProfile.getName());
+                Log.e("currentProfile",""+currentProfile.getProfilePictureUri(600, 600));
+                Toast.makeText(MainActivity.this,"Welcome, "+currentProfile.getName(),Toast.LENGTH_SHORT).show();
+            }
+        };
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,37 +92,7 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        // Intialize Facebook
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        callbackManager = CallbackManager.Factory.create();
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-
-                        Log.e("FB Login result",loginResult.
-                                toString());
-
-
-                        Toast.makeText(MainActivity.this, "Login Sucess", Toast.LENGTH_LONG).show();
-                        SharedPreferences preferences = getSharedPreferences("user_login", 0);
-                        preferences.edit().putBoolean("isUserLoggedIn", true).commit();
-                        Intent intent = new Intent(MainActivity.this, DrawerActivity.class);
-                        startActivity(intent);
-                        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        Toast.makeText(MainActivity.this, "Login Cancel", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                });
-
+        initScoialLogins();
         setContentView(R.layout.activity_main);
 
         mViewPager = (SCViewPager) findViewById(R.id.viewPager);
@@ -181,21 +194,35 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
         TextView txtGoogleLogin = (TextView) loginView.findViewById(R.id.txtGoogleLogin);
         txtGoogleLogin.setTypeface(Functions.getTypeFace(MainActivity.this));
+
+            /*SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+            signInButton.setSize(SignInButton.SIZE_STANDARD);
+            signInButton.setScopes(gso.getScopeArray());
+            signInButton.setOnClickListener(this);*/
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        //handles Google+ Result
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }//handles Facebook Result
+        else {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.linearFbLogin:
 
-                break;
-        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        profileTracker.stopTracking();
     }
 
     @Override
@@ -203,17 +230,90 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         switch (rippleView.getId()) {
             case R.id.facebookRipple:
 
-                LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("public_profile", "user_friends"));
+                facebookSingIn();
 
                 break;
             case R.id.googleRipple:
-                SharedPreferences preferences = getSharedPreferences("user_login", 0);
+               /* SharedPreferences preferences = getSharedPreferences("user_login", 0);
                 preferences = getSharedPreferences("user_login", 0);
                 preferences.edit().putBoolean("isUserLoggedIn", true).commit();
                 Intent intent = new Intent(MainActivity.this, DrawerActivity.class);
                 startActivity(intent);
-                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);*/
+                googleSignIn();
                 break;
         }
     }
+
+    private void facebookSingIn() {
+
+        LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("email","public_profile", "user_friends"));
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+                        GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(JSONObject user, GraphResponse response) {
+                                        if (response.getError() != null) {
+                                            // handle error
+                                        } else {
+                                            try{
+                                                JSONObject json = response.getJSONObject();
+                                                Log.e("#### FB RESPONSE",json.toString());
+                                            }catch (Exception e){}
+
+                                        }
+                                    }
+                                }).executeAsync();
+
+
+                        Toast.makeText(MainActivity.this, "Login Sucess", Toast.LENGTH_LONG).show();
+
+                        /*SharedPreferences preferences = getSharedPreferences("user_login", 0);
+                        preferences.edit().putBoolean("isUserLoggedIn", true).commit();
+                        Intent intent = new Intent(MainActivity.this, DrawerActivity.class);
+                        startActivity(intent);
+                        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);*/
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(MainActivity.this, "Login Cancel", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.e("GOOGLE SIGN IN", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            Log.e("GOOGLE,ID",""+acct.getId());
+            Log.e("GOOGLE,Email",""+acct.getEmail());
+            Log.e("GOOGLE,Photo url",""+acct.getPhotoUrl());
+            Log.e("GOOGLE,Display Name",""+acct.getDisplayName());
+            Toast.makeText(MainActivity.this,"Welcome, "+acct.getDisplayName(),Toast.LENGTH_SHORT).show();
+          //  mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            //updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+           // updateUI(false);
+        }
+    }
+
+    private void googleSignIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    //end of class
 }
